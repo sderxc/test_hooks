139853827e18c3a77c01b7a705a62f20441cc71b
Денис Беляев <sderxc@gmail.com>
2018-10-23 20:23:58 +0300

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-git status
-# setup_git() {
-#   git config --global user.email "sderxc@gmail.com "
-#   git config --global user.name "sderxc"
-# }
 
-# commit_website_files() {
-#   git checkout -b release
-#   git add ./build
-#   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-# }
+setup_git() {
+  git config --global user.email "sderxc@gmail.com"
+  git config --global user.name "sderxc"
+}
 
-# upload_files() {
-#   git push origin HEAD
-# }
+commit_website_files() {
+  git checkout -b release
+  git add ./build
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
 
-# setup_git
-# commit_website_files
-# upload_files
+upload_files() {
+  git push origin HEAD
+}
+
+setup_git
+commit_website_files
+upload_files
